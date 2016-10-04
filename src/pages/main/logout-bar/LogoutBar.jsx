135f11d7cc589bff273db561029f6f21f293558f
Arne Mæhlum <arne.mahlum@gmail.com>
2016-10-04 10:38:43 +0200

    MainPage: move stuff to LogoutBar

---
+++ src/pages/main/logout-bar/LogoutBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'components/button/Button';
+import { FIREBASE_APP } from 'MyFirebase';
+
+import './LogoutBar.scss';
+
+export default class LogoutBar extends React.Component {
+
+    state = {
+        logoutErrorMessage: ''
+    }
+
+    onLogOut = () => {
+        const logoutSuccess = () => {
+            console.log('logged out');
+        };
+        const logoutError = (error) => {
+            this.setState({ logoutErrorMessage: error.message });
+        };
+
+        FIREBASE_APP.auth().signOut().then(logoutSuccess, logoutError);
+    }
+
+    render() {
+        return (
+            <div className="logout-bar">
+                <Button className="button-log-out" onClick={this.onLogOut}>
+                    Log out
+                </Button>
+
+                <div className="error-message">{this.state.logoutErrorMessage}</div>
+            </div>
+        );
+    }
+}
